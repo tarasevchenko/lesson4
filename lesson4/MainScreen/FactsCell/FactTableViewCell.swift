5fc605c105a3907bd28235a62e64a3d25d11d408
Taras Evchenko <taras.evchenko@gmail.com>
2021-11-08 17:00:33 +0400

    add tableview

---
+++ lesson4/MainScreen/FactsCell/FactTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  FactTableViewCell.swift
+//  lesson4
+//
+//  Created by Тарас Евченко on 08.11.2021.
+//
+
+import UIKit
+
+class FactTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var factLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func cunfigure(factText: String) {
+        factLabel.text = factText
+    }
+}
--- lesson4/MainScreen/MainViewController.swift
+++ lesson4/MainScreen/MainViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var resultLabel: UILabel!
     
     private let viewModel = MainViewModel()
+    
+    private var facts = [Fact]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,15 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func getDataButton(_ sender: Any) {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(
+            UINib(
+                nibName: "FactTableViewCell", bundle: .main
+            ),
+            forCellReuseIdentifier: "myCell"
+        )
+        
         viewModel.getCatsData()
     }
     
@@ -29,7 +41,13 @@ extension MainViewController: MainViewModelDelegate {
     func dataDidReciveCatsData(data: [Fact]) {
         DispatchQueue.main.async {[weak self] in
             self?.resultLabel.textColor = .black
-            self?.resultLabel.text = data.randomElement()?.text
+            self?.resultLabel.text = "Данные загружены"
+            self?.facts = data
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {[weak self] in
+                self?.tableView.reloadData()
+                self?.resultLabel.isHidden = true
+            }
         }
     }
     
@@ -37,6 +55,23 @@ extension MainViewController: MainViewModelDelegate {
         DispatchQueue.main.async {[weak self] in
             self?.resultLabel.textColor = .red
             self?.resultLabel.text = "Ошибка сети"
+        }
+    }
+    
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return facts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as? FactTableViewCell {
+            cell.cunfigure(factText: facts[indexPath.row].text)
+            return cell
+        } else {
+            return UITableViewCell()
         }
     }
     
